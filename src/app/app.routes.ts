225599d061552e023de29bb408996161367043bb
Santiago Alejandro Gaviria Murcia <69436478+santialejo97@users.noreply.github.com>
2024-09-16 19:32:52 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    title: 'Login',
+    loadComponent: () => import('./auth/auth.component'),
+  },
+  {
+    path: 'dashboard',
+    title: 'Home',
+    loadComponent: () => import('./dashboard/dashboard.component'),
+    children: [
+      {
+        path: 'minute',
+        title: 'Minute',
+        loadComponent: () =>
+          import('./dashboard/pages/minute/minute.component'),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
---
+++ src/app/dashboard/pages/minute/minute.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minute',
+  standalone: true,
+  imports: [],
+  templateUrl: './minute.component.html',
+  styleUrl: './minute.component.css',
+})
+export default class MinuteComponent {}
